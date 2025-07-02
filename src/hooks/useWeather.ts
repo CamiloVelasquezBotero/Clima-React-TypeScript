6b863fb10624ffe52231046fa212f9d05b62c6bc
Camilo <camilox168@outlook.com>
2025-07-02 12:34:28 -0500

    Fixing Url

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -60,7 +60,7 @@ export default function useWeather() {
         setLoading(true)
         try {
             // Obtenemos primero Latitud y Longitud para hacer la llamada del clima
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             const { data } = await axios(geoUrl) /* (.get) Por default */
 
             //Comprobamos si existe
